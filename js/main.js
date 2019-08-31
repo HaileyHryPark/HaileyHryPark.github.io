c0eb5ffc540d0750b6bbac23790b15fc870fcf41
Hailey Park <16parkha@gmail.com>
2019-09-01 00:07:45 +0800

    first commit in vue

--- js/main.js
+++ js/main.js
@@ -3,9 +3,13 @@ function requirePassword(){
     alert(pwPrompt);
 }
 
-var editBtn = document.getElementById('edit-btn');
-editBtn.addEventListener('click', requirePassword);
+// $("#edit-btn").addEventListener('click', requirePassword);
+
+/*var editBtn = document.getElementById('edit-btn');
+editBtn.addEventListener('click', requirePassword);*/
+
 
 $(document).ready(function(){
-
+    // $("#edit-btn").addEventListener('click', requirePassword);
+    $("#edit-btn").on('click', requirePassword);
 });
